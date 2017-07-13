4abfa04d05819db67c1863fa322a7f4c73c36545
Chris Carlson <ccarlson@panth.com>
2017-07-13 09:55:39 -0400

    Adding missing MONAHRQAssemblyInfo.cs file

---
+++ Source/MONAHRQAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Agency for Healthcare Research and Quality (AHRQ)")]
+[assembly: AssemblyProduct("Monahrq® 7")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("™")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("7.1.*")]
+[assembly: AssemblyFileVersion("7.1.0.0")]
+[assembly: AssemblyInformationalVersion("7.1.*")]
